8e41fd28fabe06ef1dba9812607747f1a9b64948
jjesusfilho <jjesusfilho@gmail.com>
2018-01-22 11:30:25 +0000

    add databases to the package

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Data with São Paulo state cities and codes
+#'
+#' This is data with São Paulo state cities and their respective codes
+#' to use with the other functions
+#'
+#' @format A data frame with 645 rows and 2 variables:
+#' \describe{
+#'   \item{code}{City's code, from 1 to 645}
+#'   \item{city}{City's name}
+#'   ...
+#' }
+"cities"
+
+
+#' Data with São Paulo police stations
+#'
+#' This is data with São Paulo state police stations to be used
+#' with other functions
+#'
+#' @format A data frame with 1465 rows and 2 variables:
+#' \describe{
+#'   \item{code}{City's code, from 1 to 645}
+#'   \item{police_station}{police stations' name}
+#'   ...
+#' }
+"police_stations"
+
+
